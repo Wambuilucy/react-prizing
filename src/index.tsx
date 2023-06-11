f2a9d86585061bd609d2fde9ba472abb7b87b96b
lucy <wambuilucie99@gmail.com>
2023-06-11 21:39:47 +0300

    first com

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ChakraProvider } from '@chakra-ui/react'
+import ArticleList from './App';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +15,14 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+function App() {
+  // 2. Wrap ChakraProvider at the root of your app
+  return (
+    <ChakraProvider>
+      <ArticleList/>
+    </ChakraProvider>
+  )
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
